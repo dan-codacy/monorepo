d977e63bf4d023d75ed864ae2670be6b587c8ef4
Dan Putman <dan@codacy.com>
2020-01-23 07:12:14 -0500

    revert Bizz change

--- javascript/src/fizzbuzz.js
+++ javascript/src/fizzbuzz.js
@@ -1,7 +1,7 @@
 module.exports = function(input) {
     let output = ''
     if(input % 3 === 0) 
-        output += 'Bizz'
+        output += 'Fizz'
     if(input % 5 === 0) 
         output += 'Buzz'
 
