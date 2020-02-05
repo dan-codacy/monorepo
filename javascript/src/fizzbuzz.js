bcc74c70cad037d4640fa22a2fd1a210d836dcf9
Dan Putman <dan@codacy.com>
2020-02-05 12:41:03 -0500

    Block PR on quality gate

--- javascript/src/fizzbuzz.js
+++ javascript/src/fizzbuzz.js
@@ -4,6 +4,7 @@ module.exports = function(input) {
         output += 'Fizz'
     if(input % 5 === 0) 
         output += 'Buzz'
+    if(input % 99 === 0) output += 'Strange'
 
     return output.length > 0 ? output : input
 }
