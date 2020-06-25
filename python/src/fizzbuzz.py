15a977f509b9575c5e3722d09a7abe633d5d0e89
Dan Putman <dan@codacy.com>
2020-06-25 12:52:30 -0400

    adding issue in python to check for defaults

--- python/src/fizzbuzz.py
+++ python/src/fizzbuzz.py
@@ -7,4 +7,14 @@ class FizzBuzz:
         if input % 5 == 0:
             output += "Buzz"
 
-        return output if len(output) else str(input) 
+        return output if len(output) else str(input)
+    
+    def a_function(self, gotcha=[]):
+        print(gotcha)
+        gotcha.append('Really? Who thought this was a good idea?')
+    
+    def bad_func(self):
+        for i in range(20):
+            self.a_function()
+
+
